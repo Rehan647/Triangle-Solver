d45ee8d10f3a6b4c46341ec1002574a534277a15
Rehan <Rehan5068@conestogac.on.ca>
2025-02-22 21:56:38 -0500

    Equilateral triangle case

---
+++ TriangleSolverTests/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+namespace TriangleSolverTests
+{
+    [TestFixture] // NUnit annotation for test class
+    public class TriangleSolverTests
+    {
+        [Test] // NUnit annotation for test method
+        public void Test_EquilateralTriangle()
+        {
+            string expected = "Equilateral triangle";
+            string result = Triangle.AnalyzeTriangle(3, 3, 3);
+            Assert.AreEqual(expected, result);
+        }
+
+  
+
+
+
+    }
+
+}
